71b27a10668a8f599518671cd4702f38547cfa0c
damonali19 <49441285+damonali19@users.noreply.github.com>
2020-01-01 23:12:11 +0330

    Updates

---
+++ precache-manifest.ea25f344101d1ab15184b7e5322709a2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59a22400cf508fe20d36cfa3b34f6275",
+    "url": "/our_journey/index.html"
+  },
+  {
+    "revision": "5399f8ed1238518a2fe5",
+    "url": "/our_journey/static/css/main.de859f29.chunk.css"
+  },
+  {
+    "revision": "9e4c789fb5832a60c167",
+    "url": "/our_journey/static/js/2.05100d72.chunk.js"
+  },
+  {
+    "revision": "fddc3d22980223655e2e2510876618b7",
+    "url": "/our_journey/static/js/2.05100d72.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5399f8ed1238518a2fe5",
+    "url": "/our_journey/static/js/main.f4a246c0.chunk.js"
+  },
+  {
+    "revision": "f7153072683765c2fd93",
+    "url": "/our_journey/static/js/runtime-main.2859a22d.js"
+  },
+  {
+    "revision": "9160cfc7b26209b31347b85d433c0ad6",
+    "url": "/our_journey/static/media/1.9160cfc7.jpg"
+  },
+  {
+    "revision": "66906c64a5815c4bf414fd3408b307a8",
+    "url": "/our_journey/static/media/2.66906c64.jpg"
+  },
+  {
+    "revision": "1fcfded3b9f37a9b87997ed4a0d495b9",
+    "url": "/our_journey/static/media/3.1fcfded3.jpg"
+  }
+]);
